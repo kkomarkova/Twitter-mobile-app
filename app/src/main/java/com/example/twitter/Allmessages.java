4332b7c2c1eb29e8f67a2662b4c413dc7c7b6547
kkomarkova <58290791+kkomarkova@users.noreply.github.com>
2021-03-08 14:27:47 +0100

    All messages java and xml to be able to see all messages from API

--- app/src/main/java/com/example/twitter/Allmessages.java
+++ app/src/main/java/com/example/twitter/Allmessages.java
@@ -1,5 +1,6 @@
 package com.example.twitter;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -7,10 +8,27 @@ import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class Allmessages extends AppCompatActivity {
+    private static final String LOG_TAG = "MYMESSAGES";
+    private TextView messageView;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +36,104 @@ public class Allmessages extends AppCompatActivity {
         setContentView(R.layout.activity_allmessages);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        //Find by id
+        messageView = findViewById(R.id.mainMessageTextView);
+        progressBar = findViewById(R.id.mainProgressbar);
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //   Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                //          .setAction("Action", null).show();
+                //Add intent to add new Message
+                Intent intent = new Intent(Allmessages.this, Addnewmessage.class);
+                startActivity(intent);
+            }
+        });
+        SwipeRefreshLayout refreshLayout = findViewById(R.id.mainSwiperefresh);
+        refreshLayout.setOnRefreshListener(() -> {
+            getAndShowAllMessages();
+            refreshLayout.setRefreshing(false);
+        });
+    }
+        @Override
+        protected void onStart() {
+            super.onStart();
+            getAndShowAllMessages();
+        }
+    private void getAndShowAllMessages() {
+        MessageService bookStoreService = ApiUtils.getMessageService();
+        Call<List<Message>> getAllMessagesCall = bookStoreService.getAllMessages();
+        messageView.setText("");
+        progressBar.setVisibility(View.VISIBLE);
+
+        getAllMessagesCall.enqueue(new Callback<List<Message>>() {
+            @Override
+            public void onResponse(Call<List<Message>> call, Response<List<Message>> response) {
+                //ResponseBody body = response.raw();
+                Log.d(LOG_TAG, response.raw().toString());
+                /*try {
+                    Thread.sleep(5000);
+                    // sleep a little to get a chance to see the progressbar in action
+                    // don't do this at home
+                } catch (InterruptedException e) {
+                }*/
+                progressBar.setVisibility(View.INVISIBLE);
+                if (response.isSuccessful()) {
+                    List<Message> allMessages = response.body();
+                    Log.d(LOG_TAG, allMessages.toString());
+                    populateRecyclerView(allMessages);
+                } else {
+                    String message = "Problem " + response.code() + " " + response.message();
+                    Log.d(LOG_TAG, message);
+                    messageView.setText(message);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Message>> call, Throwable t) {
+                progressBar.setVisibility(View.INVISIBLE);
+                Log.e(LOG_TAG, t.getMessage());
+                messageView.setText(t.getMessage());
             }
         });
     }
+    private void populateRecyclerView(List<Message> allMessages) {
+        RecyclerView recyclerView = findViewById(R.id.mainRecyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        RecyclerViewSimpleAdapter<Message> adapter = new RecyclerViewSimpleAdapter<>(allMessages);
+        recyclerView.setAdapter(adapter);
+        adapter.setOnItemClickListener((view, position, item) -> {
+            Message message = (Message) item;
+            Log.d(LOG_TAG, item.toString());
+            Intent intent = new Intent(Allmessages.this, SingleMessageActivity.class);
+            intent.putExtra(SingleMessageActivity.MESSAGE, message);
+            Log.d(LOG_TAG, "putExtra " + message.toString());
+            startActivity(intent);
+        });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
