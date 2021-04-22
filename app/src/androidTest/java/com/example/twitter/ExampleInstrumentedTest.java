3b55e4287858902d6069dab6b8a2241e371012f7
Katerina Komarkova <katerina.komarkova@hotmail.com>
2021-04-22 09:56:03 +0200

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/com/example/twitter/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/twitter/ExampleInstrumentedTest.java
@@ -1,20 +1,29 @@
 package com.example.twitter;
 
+import android.view.View;
+import android.widget.EditText;
+
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 
+import org.hamcrest.Matcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withHint;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -41,20 +50,28 @@ public ActivityScenarioRule rule =
         //3.Use a ViewAction to perform an action: .perform(click())
         //See if there is text Twister App in the app
         onView(withText("Twister App")).check(matches(isDisplayed()));
-        //TextView contains
-        //onView(withId(R.id.activity_main_passwordEditText)).check(matches(withText("Password")));
-        //onView(withId(R.id.activity_main_emailEditText)).check(matches(withText("Email")));
-
-        //Test the EditText main
         
         //Button action test if they work on click
         onView(withId(R.id.activity_main_loginButton)).perform(click());
         onView(withId(R.id.activity_main_logoutButton)).perform(click());
-
+        
+       //I use NOT sentense to test that something is not corresponding with Hamcrest matching
+        onView(allOf(withId(R.id.activity_main_loginButton), not(withText("Sign-out"))));
     }
 
 
-
-
-
+    // Looks for an EditText with id = "R.id.emaileditext"
+    // Types the text "Email" into the EditText
+    // Verifies the EditText has text "Email"
+    @Test
+    public void validateEditTextEmail() {
+        onView(withId(R.id.activity_main_emailEditText)).perform(typeText("Email")).check(matches(withText("Email")));
+    }
+    // Looks for an EditText with id = "R.id.passwordeditext"
+    // Types the text "Email" into the EditText
+    // Verifies the EditText has text "Email"
+    @Test
+    public void validateEditTextPassword() {
+        onView(withId(R.id.activity_main_passwordEditText)).perform(typeText("Password")).check(matches(withText("Password")));
+    }
 }
